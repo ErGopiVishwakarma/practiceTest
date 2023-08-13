daf6e7def6c83f6239ef491f9c27518cf5d607ad
Gopi vishwakarma <g2468vi@gmail.com>
2023-08-13 17:30:54 +0530

    code added

---
+++ recursion/removeDuplicateFromString.js
@@ -0,0 +1,18 @@
+// let str = 'abbccda'
+// let removeDuplicate = (str,ind,newString) =>{
+//     if(str.length === ind){
+//         return newString
+//     }
+
+// }
+// let st = 'ab'
+// console.log(st.codePointAt())
+const sum = ()=>{
+    console.log(arguments)
+    if(arguments){
+        console.log('hii')
+    }else{
+        console.log('nahi')
+    }
+}
+sum('string')
