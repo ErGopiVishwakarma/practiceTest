34f26946d699175ebcd04985e865677d6a94a751
Gopi vishwakarma <g2468vi@gmail.com>
2023-08-13 17:23:50 +0530

    code added

---
+++ Coding/gopi.js
@@ -0,0 +1,10 @@
+let gopi = document.getElementById('input')
+let g = document.getElementById('btm')
+g.addEventListener('click',()=>{
+    console.log(gopi.value)
+})
+// console.log(gopi.value)
+
+// gopi.addEventListener('change',(e)=>{
+//     console.log(e)
+// })
